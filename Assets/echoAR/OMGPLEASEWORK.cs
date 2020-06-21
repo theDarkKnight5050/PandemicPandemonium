31ce7d26f91aa54877e94e974a309dad4b362e8d
Ahmed <aiabdalla5050@gmail.com>
2020-06-20 21:32:28 -0700

    jic
    jic

---
+++ Assets/echoAR/OMGPLEASEWORK.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OMGPLEASEWORK : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //omg u susck
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update later pls
+    }
+}
