0afc6183b0b079664be02667c5b6398d252f2f87
miramoose <mira_jambusaria@yahoo.com>
2020-06-21 06:06:13 -0500

    Android_app_attempt
    I'm trying to get it to work on the android app and get it to react to events.

--- Assets/echoAR/CustomBehaviour.cs
+++ Assets/echoAR/CustomBehaviour.cs
@@ -1,11 +1,23 @@
-﻿/**************************************************************************
+﻿/*
+ *
+ * Filename             CustomBehavior.cs 
+ * Date                 6/21/2020
+ * Author               Mira Jambusaria 
+ * Version              def-hacks 2020
+ * Copyright            2020, All Rights Reserved 
+ *************************************************************************
 * Copyright (C) echoAR, Inc. 2018-2020.                                   *
 * echoAR, Inc. proprietary and confidential.                              *
 *                                                                         *
 * Use subject to the terms of the Terms of Service available at           *
 * https://www.echoar.xyz/terms, or another agreement                      *
 * between echoAR, Inc. and you, your company or other organization.       *
-***************************************************************************/
+**************************************************************************
+ *
+ * Description              Edit this script to create any behaviours for echoAR sprites 
+ *
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +36,7 @@ public class CustomBehaviour : MonoBehaviour
     void Start()
     {
         // Add RemoteTransformations script to object and set its entry
+        //this component is responsible for enabling real-time updates 
         this.gameObject.AddComponent<RemoteTransformations>().entry = entry;
 
         // Qurey additional data to get the name
@@ -38,6 +51,11 @@ public class CustomBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        string value = "";
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("scale", out value))
+        {
+            // Set name
+            this.gameObject.scale = value * 2;
+        }   
     }
 }
