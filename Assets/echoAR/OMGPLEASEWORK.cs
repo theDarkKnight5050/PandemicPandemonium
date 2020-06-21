4904ddbbc90994829f946e70866fe845b9903d5d
David <dasun@ucdavis.edu>
2020-06-20 20:22:33 -0700

    DUUDE
    ddfadf

--- Assets/echoAR/OMGPLEASEWORK.cs
+++ Assets/echoAR/OMGPLEASEWORK.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,12 @@ public class OMGPLEASEWORK : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //omg u susck
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //update later pls
     }
 }
