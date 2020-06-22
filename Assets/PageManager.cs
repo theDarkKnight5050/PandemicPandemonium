c4b075d6be423a1ce87b06cc28ae2ef140481294
miramoose <mira_jambusaria@yahoo.com>
2020-06-21 21:27:47 -0500

    Update PageManager.cs

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿/*
+ *
+ * Filename             PageManager.cs
+ * Date                 6/21/2020
+ * Author               Mira Jambusaria 
+ * Version              def-hacks 2020
+ * Copyright            2020, All Rights Reserved 
+ * Description          This is the class that handles the Start Page Button that moves on to the next scene yay
+ *
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
@@ -7,7 +19,6 @@ public class PageManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.loadScene("SimpleAR"); 
-
+        SceneManager.LoadScene("SimpleAR"); 
     }
 }
