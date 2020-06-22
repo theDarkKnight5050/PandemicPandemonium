14dbb93bcc327dce070d6cfc76221b86ddf49d8b
miramoose <mira_jambusaria@yahoo.com>
2020-06-21 22:19:58 -0500

    End Game Page Created
    The game over page is created

---
+++ Assets/PageManager2.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *
+ * Filename             PageManager2.cs
+ * Date                 6/21/2020
+ * Author               Mira Jambusaria 
+ * Version              def-hacks 2020
+ * Copyright            2020, All Rights Reserved 
+ * Description          This is the class that handles the Game Over Button that moves on to the beginning 
+ *
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PageManager2 : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("StartMenu"); 
+    }
+}
